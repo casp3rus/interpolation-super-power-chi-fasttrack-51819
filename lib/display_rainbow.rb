0732b93500e5f9938181dc7b58f8eb09a4c2d326
Adrian Popan <adrian.popan@gmail.com>
2019-05-19 06:03:09 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -4,8 +4,8 @@ letters = ["R", "O", "Y", "G", "B", "I", "V"]
 def display_rainbow(colors)
 colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
 letters = ["R", "O", "Y", "G", "B", "I", "V"]
-  puts.chomp "#{letters[0]}:", "#{colors[0]}, ", "#{letters[1]}:", "#{colors[1]}, "
+  puts "#{letters[0]}:", "#{colors[0]}, ", "#{letters[1]}:", "#{colors[1]}, "
     
   
 end
-display_rainbow(colors)
+display_rainbow(colors).chomp

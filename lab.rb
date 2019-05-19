2b85694f12ec5974ed0596063a2d816131698e07
Adrian Popan <adrian.popan@gmail.com>
2019-05-19 05:45:11 +0000

    Automatically backed up by Learn

--- lab.rb
+++ lab.rb
@@ -1,2 +1,5 @@
 bob     = ["Bob", 46, "Juggler"]
 stefani = ["Stefani", 49, "Firefighter"]
+def print_business_card(person)
+  puts "Name: #{person[0]}, Age: #{person[1]}, Occupation: #{person[2]}"
+end
